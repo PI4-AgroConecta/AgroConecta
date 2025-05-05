36020680c7206d29686ac922351fe38793fd9c1a
Samer <samerhalat123@outlook.com>
2025-05-05 20:17:19 -0300

    adicionando acessibilidade

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
@@ -13,22 +12,12 @@ function App() {
   return (
     <Router>
       <Routes>
-        {/* Rota da home (tela inicial) */}
         <Route path="/" element={<Home />} />
-
-        {/* Rota para a página principal pós-login ou destaques */}
         <Route path="/tela-principal" element={<TelaPrincipal />} />
-
-        {/* Rotas de login */}
         <Route path="/login-cliente" element={<LoginCliente />} />
         <Route path="/login-agricultor" element={<LoginAgricultor />} />
-
-        {/* Rotas de cadastro e esqueci a senha */}
         <Route path="/cadastro" element={<Cadastro />} />
         <Route path="/esqueci-senha" element={<EsqueciSenha />} />
-
-        {/* Se quiser uma rota “catch-all” para 404, pode adicionar: */}
-        {/* <Route path="*" element={<NotFound />} /> */}
       </Routes>
     </Router>
   );
